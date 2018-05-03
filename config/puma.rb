98169bd9c10f9b81574f175e5628082eaea4522c
naraba <narabanow@gmail.com>
2018-05-03 19:39:40 +0900

    Add settings for production

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,12 @@
 app_dir = File.expand_path("../..", __FILE__)
 
-rails_env = ENV['RAILS_ENV'] || "development"
+rails_env = ENV.fetch('RAILS_ENV', 'development')
 environment rails_env
+
+workers ENV.fetch('WEB_CONCURRENCY', 2)
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+threads threads_count, threads_count
+preload_app!
 
 # socketでbindする。nginxからsocket経由で接続するため
 bind "unix:///shared/tmp/sockets/puma.sock"
